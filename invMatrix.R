ff2b0c2c888dabbd3f7c37834ad53d5269e5afdf
Dikesh.Chokshi <dikesh@cfl.rr.com>
2014-07-20 14:04:56 -0400

    added more comments.

--- invMatrix.R
+++ invMatrix.R
@@ -1,4 +1,20 @@
+# here is how to test these two functions:
+## amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+## amatrix$get()         # Returns original matrix
+## cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
+## amatrix$getinverse()  # Returns matrix inverse
+## cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
+
+## amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
+## cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
+## amatrix$get()         # Returns matrix
+## amatrix$getinverse()  # Returns matrix inverse
+
+
 makeCacheMatrix <- function (x=numeric(), ...) {
+  ## this function is a list vector with gets/set for matrix
+  ## its purpose is to cache the inverse if not already set
+  ## and return the 
   ## x is a matrix that needs inversion
   ## set the value of the matrix
   ## get the value of the matrix
@@ -14,7 +30,8 @@ makeCacheMatrix <- function (x=numeric(), ...) {
   get <- function() x
   setinverse <- function(inverse) minv <<- inverse
   
-  # modified this function solve if minv was not set via cacheSolve() first
+  # modified this function to call solve if minv was not set via cacheSolve() first
+  # this is extra and beyond what the assignment called for.
   getinverse <- function() {
     if(!is.null(minv)) {
       return(minv)
